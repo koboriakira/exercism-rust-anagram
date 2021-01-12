2e25a210c69a8a88bea8782c40f88a7ea4cd783b
Akira Kobori <private.beats@gmail.com>
2021-01-12 11:44:18 +0900

    refactor to use filter method

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,25 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let mut result: HashSet<&str> = HashSet::new();
-
     let word = word.to_lowercase();
     let word_chars = sorted(&word);
-    for &possible_anagram in possible_anagrams {
-        if word.to_lowercase() == possible_anagram.to_lowercase() {
-            continue;
-        }
-        let possible_anagram_chars = sorted(possible_anagram);
-        if word_chars.eq(&possible_anagram_chars) {
-            result.insert(possible_anagram);
-        }
-    }
-    return result;
+    possible_anagrams
+        .iter()
+        .filter(|predicate| {
+            let predicate = predicate.to_lowercase();
+            if predicate.to_lowercase() == word {
+                return false;
+            } else {
+                let possible_anagram_chars = sorted(&predicate);
+                word_chars.eq(&possible_anagram_chars)
+            }
+        })
+        .cloned()
+        .collect()
 }
 
 fn sorted(word: &str) -> Vec<char> {
-    let mut word_chars = word.to_lowercase().chars().collect::<Vec<char>>();
+    let mut word_chars = word.chars().collect::<Vec<char>>();
     word_chars.sort();
     word_chars
 }
